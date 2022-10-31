d8958f1013f2b157fe691519a057965fcdc949d8
Fabiano Cardoso <sl.biano28@gmail.com>
2022-10-31 18:09:38 -0300

    start project

---
+++ src/test/java/com/fabiano/fablog/FablogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiano.fablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FablogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
