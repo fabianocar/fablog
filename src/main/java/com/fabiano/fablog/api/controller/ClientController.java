54bbab8902eff92fcaebffcaec1260cf53b0f089
Fabiano Cardoso <sl.biano28@gmail.com>
2022-11-01 12:07:22 -0300

    create test rest controller

---
+++ src/main/java/com/fabiano/fablog/api/controller/ClientController.java
@@ -0,0 +1,12 @@
+package com.fabiano.fablog.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController {
+    @GetMapping("/client")
+    public String list() {
+       return "Teste"; 
+    }
+}
