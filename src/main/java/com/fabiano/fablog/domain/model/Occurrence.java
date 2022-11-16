8183f7b5b72b6825a0ad4f27f5299892393bcc66
Fabiano Cardoso <sl.biano28@gmail.com>
2022-11-16 13:56:12 -0300

    add Ocurrence in model and create table in MySQL

---
+++ src/main/java/com/fabiano/fablog/domain/model/Occurrence.java
@@ -0,0 +1,38 @@
+package com.fabiano.fablog.domain.model;
+
+import java.time.OffsetDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ManyToAny;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Occurrence {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Delivery delivery;
+
+    private String description;
+
+    @Column(name = "date_register")
+    private OffsetDateTime dateRegister;
+
+}
