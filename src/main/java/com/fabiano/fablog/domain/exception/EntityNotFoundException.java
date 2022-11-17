9193d90a60aba1f0fac59f9c532735775b97e4b2
Fabiano Cardoso <sl.biano28@gmail.com>
2022-11-17 16:11:55 -0300

    create EntityNotFoundException

---
+++ src/main/java/com/fabiano/fablog/domain/exception/EntityNotFoundException.java
@@ -0,0 +1,12 @@
+package com.fabiano.fablog.domain.exception;
+
+public class EntityNotFoundException extends BusinessException{
+
+    private static final long serialVersionUID = 1L;
+
+    public EntityNotFoundException(String message) {
+        super(message);
+        
+    }
+    
+}
